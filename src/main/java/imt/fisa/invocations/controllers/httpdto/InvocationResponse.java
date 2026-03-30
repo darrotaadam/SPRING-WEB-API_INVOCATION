61cd07bd20058ff4e1eb0aaab83c6fc7f16abadb
darrotaadam <adam.darrot@etu.imt-nord-europe.fr>
2026-03-30 20:57:57 +0200

    Add: basic DTO for http responses

--- src/main/java/imt/fisa/invocations/controllers/httpdto/InvocationResponse.java
+++ src/main/java/imt/fisa/invocations/controllers/httpdto/InvocationResponse.java
@@ -1,6 +1,17 @@
 package imt.fisa.invocations.controllers.httpdto;
 
 public class InvocationResponse {
+    private String monstreId;
 
+    public InvocationResponse(String monstreId) {
+        this.monstreId = monstreId;
+    }
 
+    public String getMonstreId() {
+        return monstreId;
+    }
+
+    public void setMonstreId(String monstreId) {
+        this.monstreId = monstreId;
+    }
 }
