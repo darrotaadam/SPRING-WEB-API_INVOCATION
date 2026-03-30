77655602ed9aaed7fbd4209a6b9c0e1a7c0445d0
darrotaadam <adam.darrot@etu.imt-nord-europe.fr>
2026-03-30 20:57:38 +0200

    Add: Basic error handling

--- src/main/java/imt/fisa/invocations/exceptions/GlobalExceptionHandler.java
+++ src/main/java/imt/fisa/invocations/exceptions/GlobalExceptionHandler.java
@@ -12,6 +12,18 @@ import java.util.Map;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String,Object> handleBadRequestException(BadRequestException e){
+        return Map.of(
+                "code", "400",
+                "message", e.getMessage(),
+                "timestamp", LocalDateTime.now()
+        );
+
+    }
+
+
     @ExceptionHandler(UnauthorizedException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public Map<String,Object> handleUnauthorizedException(UnauthorizedException e){
@@ -25,7 +37,7 @@ public class GlobalExceptionHandler {
 
 
     @ExceptionHandler(InternalServerErrorException.class)
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public Map<String,Object> handleInternalServerErrorException(InternalServerErrorException e){
         return Map.of(
                 "code", "500",
@@ -34,4 +46,17 @@ public class GlobalExceptionHandler {
         );
 
     }
+
+    @ExceptionHandler(NullPointerException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String,Object> handleNullPointerException(NullPointerException e){
+        return Map.of(
+                "code", "500",
+                "message", e.getMessage(),
+                "timestamp", LocalDateTime.now()
+        );
+
+    }
+
+
 }
